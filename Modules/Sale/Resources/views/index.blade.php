56a95daf0f1c770ecdc183a010747f5ca732bca3
distro104 <distro104@gmail.com>
2023-09-29 16:36:09 -0300

    fixed Struture project/Docker

--- Modules/Sale/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('sale::layouts.master')
-
-@section('content')
-    <h1>Modulo de Vendas  - TESTE</h1>
-
-    <p>
-        This view is loaded from module: {!! config('sale.name') !!}
-    </p>
-@endsection

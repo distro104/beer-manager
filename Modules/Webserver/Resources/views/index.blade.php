01deec137dd2058c794348897183be670384ccdf
distro104 <distro104@gmail.com>
2023-10-11 18:51:02 -0300

    Created new module Webserver

---
+++ Modules/Webserver/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('webserver::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('webserver.name') !!}
+    </p>
+@endsection

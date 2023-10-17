3f1dbdc08089d080ab7de13c66dc081d203ee0da
distro104 <distro104@gmail.com>
2023-10-16 23:55:06 -0300

    Recreate controllers

--- app/Http/Controllers/Access_typeController.php
+++ app/Http/Controllers/Access_typeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PaymentController extends Controller
+class Access_typeController extends Controller
 {
     /**
      * Display a listing of the resource.
